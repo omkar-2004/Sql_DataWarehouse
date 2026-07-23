6f3d66aaf7afe6a496b6ce83dcf3e21218ac2ded
omkar yadav <omkar.yadav.m.y@gmail.com>
2026-07-23 21:28:08 +0530

    Create DDL Structure for silver schema

---
+++ Scripts/silver/ddl_silver.sql
@@ -0,0 +1,119 @@
+/*
+=============================================================
+DDL Script: Create silver Table Structure
+=============================================================
+Script Purpose:
+	This script creates tables in the 'silver' schema.
+	Dropping existing tables if they already exists.
+	Run this script to redefine the DDL structure of the 'silver' tables.
+	
+WARNING:
+    Running this script will drop the entire table if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+
+use DataWarehouse;
+GO
+
+
+-- Creating Customer information table in silver schema as silver.crm_cust_info from crm datasource.
+-- if table exists it drop the table and recreate the table
+IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_cust_info;
+CREATE TABLE silver.crm_cust_info (
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+	);
+GO
+
+-- Creating product information table in silver schema as silver.crm_prd_info from crm datasource
+-- if table exists it drop the table and recreate the table
+
+IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info;
+CREATE TABLE silver.crm_prd_info(
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt datetime,
+	prd_end_dt datetime,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+	);
+GO
+
+-- Creating sales details table in silver schema as silver.crm_sls_details from crm datasource
+-- if table exists it drop the table and recreate the table
+
+IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details(
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+-- Creating customer birthdate table in silver schema as silver.erp_CUST_AZ12 from erp datasource
+-- if table exists it drop the table and recreate the table
+
+IF OBJECT_ID ('silver.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE silver.erp_cust_az12;
+CREATE TABLE silver.erp_cust_az12(
+	cid NVARCHAR(50),
+	bdate DATETIME,
+	gen NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+-- Creating customer country location table in silver schema as silver.erp_LOC_A101 from erp datasource
+-- if table exists it drop the table and recreate the table
+
+IF OBJECT_ID ('silver.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE silver.erp_loc_a101;
+CREATE TABLE silver.erp_loc_a101(
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+-- Creating product Category table in silver schema as silver.erp_PX_CAT_G1V2 from erp datasource
+-- if table exists it drop the table and recreate the table
+
+IF OBJECT_ID ('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE silver.erp_px_cat_g1v2;
+CREATE TABLE silver.erp_px_cat_g1v2(
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+
+
+
+
+
+
+
+
