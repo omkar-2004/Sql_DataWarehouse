169e7d6102719cef5ba5b1bd293212b18d5195f7
omkar yadav <omkar.yadav.m.y@gmail.com>
2026-07-24 16:05:51 +0530

    cat_id column add in silver product information table

--- Scripts/silver/ddl_silver.sql
+++ Scripts/silver/ddl_silver.sql
@@ -41,12 +41,13 @@ IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info(
 	prd_id INT,
+	cat_id NVARCHAR(50),
 	prd_key NVARCHAR(50),
 	prd_nm NVARCHAR(50),
 	prd_cost INT,
 	prd_line NVARCHAR(50),
-	prd_start_dt datetime,
-	prd_end_dt datetime,
+	prd_start_dt DATE,
+	prd_end_dt DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 	);
 GO
