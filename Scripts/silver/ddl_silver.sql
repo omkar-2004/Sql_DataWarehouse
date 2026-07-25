ba5f5f15906a671c80af4c2d9f0330e7800478ee
omkar yadav <omkar.yadav.m.y@gmail.com>
2026-07-25 17:52:11 +0530

    Change DateTime to DATE in silver.erp_cust_az12 dll

--- Scripts/silver/ddl_silver.sql
+++ Scripts/silver/ddl_silver.sql
@@ -78,7 +78,7 @@ IF OBJECT_ID ('silver.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12;
 CREATE TABLE silver.erp_cust_az12(
 	cid NVARCHAR(50),
-	bdate DATETIME,
+	bdate DATE,
 	gen NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
